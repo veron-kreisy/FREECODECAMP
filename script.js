847f68906df9fec4b781ca61e93d56e210cbaf2c
miss_codein <mbula0563@gmail.com>
2024-02-04 22:02:00 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
- function Togglemenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-
-
-
- }
